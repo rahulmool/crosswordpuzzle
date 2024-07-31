f5de6091bf2aa8ff30605b198a42235c4787330a
Rahul Mool <rahulmool3@gmail.com>
2024-08-01 01:37:25 +0530

    updated login page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -9,22 +10,23 @@ import { NavController, ToastController } from '@ionic/angular';
 export class LoginPage implements OnInit {
   username:string;
   password:string;
-  constructor(public navCtrl: NavController,public toast:ToastController) { }
+  constructor(public navCtrl: NavController,public toast:ToastController,private alertController: AlertController) { }
 
   ngOnInit() {
   }
   fun(){
-    if(this.username==="admin"&& this.password==="adm@123"){
+    if(this.username==="admin"&& this.password==="1234"){
       console.log("suceessful login as admin");
       this.username=""
       this.password=""
       this.navCtrl.navigateForward('/editcrossword');
-    }else if(this.username==="user"&& this.password==="usr@123"){
+    }else if(this.username==="user"&& this.password==="1234"){
       this.username=""
       this.password=""
       this.navCtrl.navigateForward('/test');
     }else{
       this.openToast("Please enter a valid username and password")
+      this.presentAlert();
     }
     console.log(this.username,this.password);
   }
@@ -37,4 +39,14 @@ export class LoginPage implements OnInit {
     toast.present();  
   }  
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Please Login as admin or user',
+      subHeader: 'Password for both admin and user is 1234',
+      message: 'i.e username:admin , password:1234 and username:user , password=1234.',
+      buttons: ['Close'],
+    });
+
+    await alert.present();
+  }
 }
